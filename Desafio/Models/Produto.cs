1c0163e38bbe3bf3032eac3e98951975e7f62ef3
Cleyslane <cleyslane.silva@souunit.com>
2020-07-26 19:09:33 -0300

    Criação da classe produto, do controller e adição do contexto do banco

---
+++ Desafio/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public float ValorUnitario { get; set; }
+        public int QtdEstoque { get; set; }
+    }
+}
---
+++ Desafio/Models/ProdutoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio.Models
+{
+    public class ProdutoContext : DbContext
+    {
+        public ProdutoContext(DbContextOptions<ProdutoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
