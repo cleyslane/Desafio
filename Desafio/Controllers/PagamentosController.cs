d90ccd9a7ee298512e12ab81c5565d552b427169
Cleyslane <cleyslane.silva@souunit.com>
2020-07-28 01:46:06 -0300

    API de pagamentos

---
+++ Desafio/Controllers/PagamentosController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Desafio.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio.Controllers
+{
+    public class PagamentosController : ControllerBase
+    {
+        private readonly ProdutoContext _context;
+
+        public PagamentosController(ProdutoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public ActionResult PostPagmentos([FromBody]float value, CartaoCredito cartao)
+        {
+            Pagamento pag = null;
+
+            pag.valor = value;
+            pag.cartao = cartao;
+
+            if (pag.valor > 100.0)
+            {
+                pag.estado = "APROVADO";
+                return Ok(pag.estado);
+            }
+
+            pag.estado = "REJEITADO";
+            return BadRequest(pag.estado);
+
+
+        }
+    }
+}
---
+++ Desafio/Models/Pagamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio.Models
+{
+    public class Pagamento
+    {
+        [Required]
+        public float valor { get; set; }
+        [Required]
+        public CartaoCredito cartao { get; set; }
+        public String estado { get; set; }
+    }
+}
